9f796c86de8bf4292327e48ac0233dc5d1a872c5
Gone Die <onejrcoc@gmail.com>
2020-04-07 02:08:44 +0700

    test respown

--- config/session.php
+++ config/session.php
@@ -126,7 +126,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME')).'_session'
+        Str::slug(env('APP_NAME', 'arcane'), '_').'_session'
     ),
 
     /*
